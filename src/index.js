bb6c8cc88ed67e8adc3c78e51d3cc8ef4bd5d8a8
zyphorixx <24256@iiitu.ac.in>
2026-01-02 22:18:44 +0530

    Added error files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const { apiRoutes } = require('./routes/index');
 
 const { PORT } = require('./config/serverConfig');
+const db = require('./models/index');
 
 const setupAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
 
+    app.use('/api', apiRoutes);
+
     app.listen(PORT, () => {
         console.log(`Server started at PORT ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter : true});
+        }
     });
 }
 
---
+++ src/utils/error/service-error.js
@@ -0,0 +1,19 @@
+const { StatusCodes } = require('http-status-codes');
+
+class ServiceError extends Error {
+    constructor(
+        message = 'Something went wrong',
+        explanation = 'Service Layer error',
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    ) {
+        super(message); 
+        
+        this.name = 'ServiceError';
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = ServiceError;
